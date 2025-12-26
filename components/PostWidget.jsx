e8b5398958ca160230386b9b32ef85f4f4532f3b
audrbar <audrbar@gmail.com>
2025-12-26 22:25:08 +0200

    Fix: Image component width/height props (remove px units, use numeric values)

--- components/PostWidget.jsx
+++ components/PostWidget.jsx
@@ -26,8 +26,8 @@ const PostWidget = ({ categories, slug }) => {
             <Image
               loader={grpahCMSImageLoader}
               alt={post.title}
-              height="60px"
-              width="60px"
+              height={60}
+              width={60}
               unoptimized
               className="align-middle rounded-full"
               src={post.featuredImage.url}
